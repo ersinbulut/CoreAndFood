e2437397d1172315d0782614b0ba20553065baac
ersinbulut <ersinblt61@gmail.com>
2021-01-15 20:03:57 +0300

    Add Food Design

--- CoreAndFood/Controllers/FoodController.cs
+++ CoreAndFood/Controllers/FoodController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreAndFood.Data.Models;
 using CoreAndFood.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,21 @@ namespace CoreAndFood.Controllers
 {
     public class FoodController : Controller
     {
+        FoodRepository foodRepository = new FoodRepository();
         public IActionResult Index()
         {
-            FoodRepository foodRepository = new FoodRepository();
             return View(foodRepository.TList("Category"));
+        }
+        [HttpGet]
+        public IActionResult FoodAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FoodAdd(Food p)
+        {
+            foodRepository.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
