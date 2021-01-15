2f66c12bdb1ed546e6396d7bb5f7665127c8ebb4
ersinbulut <ersinblt61@gmail.com>
2021-01-15 19:48:54 +0300

    Add Category

--- CoreAndFood/Controllers/CategoryController.cs
+++ CoreAndFood/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreAndFood.Data.Models;
 using CoreAndFood.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,22 @@ namespace CoreAndFood.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryRepository categoryRepository = new CategoryRepository();
         public IActionResult Index()
         {
-            CategoryRepository categoryRepository = new CategoryRepository();
             return View(categoryRepository.TList());
         }
+        [HttpGet]
+        public IActionResult CategoryAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            categoryRepository.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
     }
 }
